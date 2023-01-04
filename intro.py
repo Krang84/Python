dcb7de54107eb81be01a8431a6d394045f45c5f2
Krang84 <patelassad@gmail.com>
2023-01-04 21:19:35 +0100

    Update intro.py

--- intro.py
+++ intro.py
@@ -1,23 +1,32 @@
-# comments
+# Python 101
 
-# print() 
+# https://www.youtube.com/channel/UCVFpMcuKcw59wWSERqFUSJg 
 
-# print("string")
+# 1.print("string")
 
+# How to !!
 print("hello")
 
-# data strings
+# ex 1
 
-# operator +
+print("hello codeacademy")
 
+
+# 2.operator + and print()
+
+# "hello" + "john titor 42"
 print("hello" + "john titor 42")
 
 print("a"+"b)
 # "ab"
 
+# 3.Errors
+      
 # Error 1 : EOL => SyntaxError (missing one quote)
 print("abvdbcbc)
+      
 # EOL : Quotes dont match
 print("a')
+      
 # Error 2: NameError => missing quotes
 print(github)
