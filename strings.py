2ed570616e1baf27c2cd3d3924914e153dc3f694
Krang84 <patelassad@gmail.com>
2023-01-12 20:13:53 +0100

    Create strings.py

---
+++ strings.py
@@ -0,0 +1,24 @@
+brian="Hello life!"
+print(brian)
+
+caesar="Graham"
+praline="John"
+viking="Teresa"
+
+# Put your variables above this line
+
+print(caesar)
+print(praline)
+print(viking)
+
+fifth_letter = 'MONTY'[4]
+
+parrot = "Norwegian Blue"
+print(len(parrot))
+
+print(fifth_letter)
+
+
+# string.method()
+print(parrot.lower())
+print(parrot.upper())
