96795a44421f6092c79d95d32609e80354c23da9
Krang84 <patelassad@gmail.com>
2026-01-13 10:57:10 +0100

    Update poo1.py

--- poo1.py
+++ poo1.py
@@ -1,10 +1,10 @@
 
->>> class MaClasse():
-...   pass
-... 
->>> instance=MaClasse()
->>> print(instance)
-<__main__.MaClasse object at 0x70c9b93035c0>
->>> 
+class MaClasse():
+  pass
+ 
+instance=MaClasse()
+print(instance)
+#<__main__.MaClasse object at 0x70c9b93035c0>
+
 
 
